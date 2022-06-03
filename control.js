07451d1c818e6687ff335f7d40dcc4ead372dc0e
Architolb <brandon_tolbert@outlook.com>
2022-06-03 00:19:32 -0500

    first commit

---
+++ control.js
@@ -0,0 +1,27 @@
+console.log(document);
+
+// let getPokemon = name => {
+//     let response = axios.get('https://pokeapi.com/api/v2/pokemon/${name}');
+//     console.log(response);
+//     return response
+
+// }
+
+// let ourData = getPokemon('machamp');
+// console.log(ourData.data.name);
+
+// let loadPokemon = async name => {
+//     let data = await letPokemon(name);
+//     console.log(data);
+//     console.log(data.name);
+//     let x = document.createElement('p')
+//     x.innerHTML = data.name;
+//     document.body.append(x);
+
+// }
+
+
+// loadPokemon('machamp')
+
+// let clicky = document.querySelector('#clicky');
+// clicky.addEventListener('click', ()=>{loadPokemon('spheal')})
